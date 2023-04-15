5908b552eaa1a8941c0bf37a39ad90d3b99c082e
C-Hurtmann <constantine2903@gmail.com>
2023-04-15 12:07:00 +0300

    finished factorize

--- factorize.py
+++ factorize.py
@@ -1,11 +1,15 @@
-from multiprocessing import Pool
-from concurrent.futures import ProcessPoolExecutor
+from multiprocessing.pool import Pool
 from functools import wraps
 import logging
 from time import time
 
 
-def exec_factorize(number: int) -> list[list[int]]:
+def exec_factorize(number: int) -> list[int]:
+    """Main process of solving
+
+    Returns:
+        list[int]: numbers which could be divided on number without trace
+    """
     dividables  =[]
     for i in range(1, number + 1):
         if not number % i:
@@ -23,25 +27,21 @@ def timer(func):
     return wrapper
 
 @timer
-def factorize(*numbers):
+def factorize(numbers: tuple):
     result_list = []
     for number in numbers:
         result_list.append(exec_factorize(number))
     return result_list
 
 @timer
-def multi_facrorize(*numbers: int) -> list[list[int]]:
-    with ProcessPoolExecutor() as executor:
-        result_list = tuple(executor.map(exec_factorize, numbers))
-        return result_list
+def multi_facrorize(numbers: tuple) -> list[list[int]]:
+    result_list = []
+    pool = Pool()
+    result_list = pool.map_async(exec_factorize, numbers).get()
+    return result_list
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG, format='%(message)s')
-    a, b, c, d, e  = factorize(128, 255, 99999, 10651060, 86494264)
-    ma, mb, mc, md, me = multi_facrorize(128, 255, 99999, 10651060, 86494264)
-
-    assert a == [1, 2, 4, 8, 16, 32, 64, 128]
-    assert b == [1, 3, 5, 15, 17, 51, 85, 255]
-    assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
-    assert d == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395, 532553, 760790, 1065106, 1521580, 2130212, 2662765, 5325530, 10651060]
+    numbers = (128, 255, 99999, 465466, 12316898, 2058068, 10651060)
+    print(factorize(numbers) == multi_facrorize(numbers)) # check if results is the same
